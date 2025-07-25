293f635c2fd8997e8204d7c45d77b14dc39a157a
Hwang Chaerin <rin2005@hanmail.net>
2025-07-25 21:46:38 +0900

    55

--- bible.py
+++ bible.py
@@ -4,7 +4,15 @@
 import tkinter
 import random
 
-#성경 구절 입력 및 저장
+#함수0: 좌표출력기
+def mouseMove(event):
+    x=event.x
+    y=event.y
+
+    labelMouse.config(text=str(x)+', '+str(y))
+    labelMouse.place(x=10,y=10)
+
+#함수1: 성경 구절 입력 및 저장
 def bibleWrite():
     outFile = open('sentence.txt', 'a')
     while True:
@@ -17,47 +25,51 @@ def bibleWrite():
             break            
     outFile.close()
 
-#랜덤 출력
+#함수2: 랜덤 출력
 def bibleChoose():
+    global chosen
     inFile = open('sentence.txt', 'r')
     randomBible = [line.strip() for line in inFile.readlines()]
     inFile.close()
     if randomBible!='':
         chosen = random.choice(randomBible)
-        print(chosen)
     else:
         print('파일이 비어있습니다.')    
 
-#좌표출력기
-def mouseMove(event):
-    x=event.x
-    y=event.y
+#함수3: 문장 수정 / 버튼1
+def btn_click():
+    global chosen
+    bibleChoose()
+    senLabel.config(text=chosen)
 
-    labelMouse.config(text=str(x)+', '+str(y))
-    labelMouse.place(x=743,y=567)
+#함수4: 성경구절 추가 / 버튼2
+def addBtn_click():
+    bibleWrite()
 
-#GUI => 이미지 찾아서 캔버스로 변경 예정
+#캔버스
 root = tkinter.Tk()
 root.title('성경 구절')
-root.geometry('800x600')
+canvas = tkinter.Canvas(root, width=720, height=404)
+canvas.pack()
+bgimg = tkinter.PhotoImage(file='sun.png')
+canvas.create_image(360,202,image=bgimg)
 
 #라벨
-titleLabel = tkinter.Label(root, text='오늘의 성경 구절', font=('pretendard',40))
+titleLabel = tkinter.Label(root, text='오늘의 성경 구절', font=('pretendard',30), bg='#afeedf')
 titleLabel.place(x=220,y=100)
 
-senLabel = tkinter.Label(root, text='', font=('pretendard',10))
-senLabel.place(x=100,y=300)
-
-#좌표출력기
-root.bind('<Motion>', mouseMove)
-labelMouse = tkinter.Label(root, text=', ', font=('맑은고딕',10))
-
-def btn_click():
-    print('돌리쟈!')
-    bibleChoose()
+senLabel = tkinter.Label(root, text='', font=('pretendard Bold',11), wraplength=539, bg='#afeedf')
+senLabel.place(x=100,y=280)
 
 #버튼
-Btn = tkinter.Button(root, text='Click', font=('pretendard', 25), command=btn_click)
-Btn.place(x=350,y=150)
+Btn = tkinter.Button(root, text='Click', font=('pretendard', 25), bg='#afeedf', command=btn_click)
+Btn.place(x=310,y=180)
+
+addBtn = tkinter.Button(root, text='Add Bible', font='pretendard',bg="#e4d9a4", command=addBtn_click)
+addBtn.place(x=20,y=20)
+
+#좌표출력기
+#root.bind('<Motion>', mouseMove)
+#labelMouse = tkinter.Label(root, text=', ', font=('맑은고딕',10))
 
 root.mainloop()
